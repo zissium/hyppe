87867f571f3e55071eb98b39378cefdbfb2d855a
zhouyujie@qq.com <kakajieshao@hydeMacBook-Pro.local>
2021-04-19 18:43:45 +0800

    文件提交

---
+++ wp-content/themes/mixtapewp/faq.php
@@ -0,0 +1,59 @@
+<div class="faqwrapper">
+    <div class="wholesale">
+        <h2>Wholesale Inquiry</h2>
+        <ul class="items">
+            <li>
+                1. Q: How can I become your wholesaler?
+                <p>A: Please fill out our contact form(超链接) or email 邮箱. </p>
+            </li>
+            <li>
+                2. Q: What’s your wholesale pricing.
+                <p>A: Please contact us(超链接), we will reply you ASAP.  </p>
+            </li>
+            <li>
+                3. Q: How can I purchase your products?
+                <p>A: Please fill out our contact form(超链接), our staff will get back to you and help you ASAP.</p>
+            </li>
+            <li>
+                4. Q: Can I get more information to promote your products?
+                <p>A: Sure! Please email 邮箱, and we will offer a wide variety of marketing materials to help you.</p>
+            </li>
+        </ul>
+    </div>
+    
+    <div class="general">
+        <h2>General Inquiry</h2>
+        <ul class="items">
+            <li>
+                1.Q: What is BECO BAR?
+                <p>A: BECO BAR is one of the disposable vape devices from VAPTIO brand, and it is easy to use with exceptional flavours.</p>
+            </li>
+            <li>
+                2. Q: What’s the minimum age to purchase BECO products.
+                <p>A: You must be the legal age 18+ to purchase BECO products. </p>
+            </li>
+            <li>
+                3. Q: How to use BECO products?
+                <p>A: Simply open the package, and then take a hit.</p>
+            </li>
+            <li>
+                4. Q: How long can a BECO device last? 
+                <p>A: It depends on how heavily and how often you use it. But according to the TPD, the max capacity is 2ml with about 400 puffs.</p>
+            </li>
+            <li>
+                5. Q: How to know when a BECO device is empty?
+                <p>A: When you cannot puff out anything, it is empty. But the last draw will not taste burnt at all, as our BECO device is consistent from beginning to end.</p>
+            </li>
+            <li>
+                6. Q: What Are the BECO Warranty and Return Policy? 
+                <p>A: It depends on where and when you purchased it. Please contact the retailer where your device was purchased, and provide your original purchase receipt.</p>
+            </li>
+            <li>
+                7. Q: You should improve your quality control.
+                <p>A: We love hearing how we can make our product better, and we aim to make high quality product. It will be very appreciated you can figure out where we should improve.Thanks for your support! </p>
+            </li>
+        </ul>
+
+    </div>
+
+</div>
