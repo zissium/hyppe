7221675264fadf29812ec222a667e39a3686d948
zhouyujie@qq.com <kakajieshao@hydeMacBook-Pro.local>
2021-04-23 17:27:07 +0800

    文件提交

--- wp-content/themes/mixtapewp/faq.php
+++ wp-content/themes/mixtapewp/faq.php
@@ -2,25 +2,26 @@
 
 <?php get_header(); ?>
 
+<h4 class="title">FAQ</h4>
 <div class="faqwrapper">
     <div class="wholesale">
         <h2>Wholesale Inquiry</h2>
         <ul class="items">
             <li>
                 1. Q: How can I become your wholesaler?<i class="iconfont open-submenu" style="display: block;"></i><i class="iconfont close-submenu" style="display: none;"></i>
-                <p>A: Please fill out our <a href="https://vaptiobeco.co.uk/contact">contact form</a> or email <a href="mailto:uk@vaptio.com">uk@vaptio.com</a>. </p>
+                <p>A: Please fill out our <a class="contact" href="https://vaptiobeco.co.uk/contact">contact form</a> or email <a class="email" href="mailto:uk@vaptio.com">uk@vaptio.com</a>. </p>
             </li>
             <li>
                 2. Q: What’s your wholesale pricing.<i class="iconfont open-submenu" style="display: block;"></i><i class="iconfont close-submenu" style="display: none;"></i>
-                <p>A: Please <a href="https://vaptiobeco.co.uk/contact">contact us</a>, we will reply you ASAP.  </p>
+                <p>A: Please <a class="contact" href="https://vaptiobeco.co.uk/contact">contact us</a>, we will reply you ASAP.  </p>
             </li>
             <li>
                 3. Q: How can I purchase your products?<i class="iconfont open-submenu" style="display: block;"></i><i class="iconfont close-submenu" style="display: none;"></i>
-                <p>A: Please fill out our <a href="https://vaptiobeco.co.uk/contact">contact form</a>, our staff will get back to you and help you ASAP.</p>
+                <p>A: Please fill out our <a class="contact" href="https://vaptiobeco.co.uk/contact">contact form</a>, our staff will get back to you and help you ASAP.</p>
             </li>
             <li>
                 4. Q: Can I get more information to promote your products?<i class="iconfont open-submenu" style="display: block;"></i><i class="iconfont close-submenu" style="display: none;"></i>
-                <p>A: Sure! Please email 邮箱, and we will offer a wide variety of marketing materials to help you.</p>
+                <p>A: Sure! Please email <a class="email" href="mailto:uk@vaptio.com">uk@vaptio.com</a>, and we will offer a wide variety of marketing materials to help you.</p>
             </li>
         </ul>
     </div>
