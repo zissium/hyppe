428afad62f9f4afbb2b1e331febac1b6f70160c7
zhouyujie@qq.com <kakajieshao@hydeMacBook-Pro.local>
2021-05-31 16:34:43 +0800

    文件提交

--- wp-content/themes/mixtapewp/footer.php
+++ wp-content/themes/mixtapewp/footer.php
@@ -4,98 +4,37 @@
         <div class="site-menu-m">
             <ul>
                 <li class="">
-                    <div>More Categories
-                        <i class="iconfont open-submenu" style="display: block;"></i>
-                        <i class="iconfont close-submenu" style="display: none;"></i>
-                    </div>
-                    <div class="sub-menu" style="display: none;">
-                        <a href="javascript:;">Ethernet Cables</a>
-                        <a href="javascript:;">Power Cables</a>
-                        <a href="javascript:;">In-wall Cables</a>
+                    <div>
+                        <a href="https://vaptiobeco.co.uk">Home</a>
                     </div>
                 </li>
 
                 <li class="">
-                    <div>More Cables
-                        <i class="iconfont open-submenu" style="display: block;"></i>
-                        <i class="iconfont close-submenu" style="display: none;"></i>
-                    </div>
-                    <div class="sub-menu" style="display: none;">
-                        <a href="javascript:;">Ethernet Cables</a>
+                <div>
+                    <a href="https://vaptiobeco.co.uk/about-us">About Us</a>
                     </div>
                 </li>
                 <li class="">
-                    <div>Partners' Products
-                        <i class="iconfont open-submenu" style="display: block;"></i>
-                        <i class="iconfont close-submenu" style="display: none;"></i>
-                    </div>
-                    <div class="sub-menu" style="display: none;">
-                        <a href="javascript:;">Online Store</a>
-                        <a href="javascript:;">Authorized Stores</a>
-                        <a href="javascript:;">Local Stores</a>
-                        <a href="javascript:;">Become a Dealer</a>
-                        <a href="javascript:;">Shop for Business</a>
-                        <a href="javascript:;">Co-Marketing</a>
-                    </div>
+                <div>
+                     <a href="https://vaptiobeco.co.uk/partner">Partners</a>
+                     </div>
                 </li>
                 <li class="">
-                    <div>Support
-                        <i class="iconfont open-submenu" style="display: block;"></i>
-                        <i class="iconfont close-submenu" style="display: none;"></i>
-                    </div>
-                    <div class="sub-menu" style="display: none;">
-                        <a href="https://yabaro.com/help/">Help Center</a>
-                        <a href="https://yabaro.com/after-sales-service-policies/">Aftersales Policy</a>
-                        <a href="https://yabaro.com/delivery-pickup/">Pickup Service</a>
-                        <a href="https://yabaro.com/y-plus-care/">Y+ Care</a>
+                <div>
+                    <a href="https://vaptiobeco.co.uk/wheretobuy">Where to Buy </a>
                     </div>
                 </li>
 
                 <li class="">
-                    <div>Where to Buy
-                        <i class="iconfont open-submenu" style="display: block;"></i>
-                        <i class="iconfont close-submenu" style="display: none;"></i>
-                    </div>
-                    <div class="sub-menu" style="display: none;">
-                        <a href="https://yabaro.com/online-store/">Online Store</a>
-                        <a href="javascript:;">Authorized Stores</a>
-                        <a href="javascript:;">Local Stores</a>
-                    </div>
-                </li>
-
-
-                <li class="">
-                    <div>Grow with Us
-                        <i class="iconfont open-submenu" style="display: block;"></i>
-                        <i class="iconfont close-submenu" style="display: none;"></i>
-                    </div>
-                    <div class="sub-menu" style="display: none;">
-                       <a href="https://yabaro.com/become-a-dealer">Become a Dealer</a>
-                    </div>
+                <div>
+                   <a href="https://vaptiobeco.co.uk/faq">FAQ</a>
+                   </div>
                 </li>
 
                 <li class="">
-                    <div>For Business
-                        <i class="iconfont open-submenu" style="display: block;"></i>
-                        <i class="iconfont close-submenu" style="display: none;"></i>
-                    </div>
-                    <div class="sub-menu" style="display: none;">
-                       <a href="https://yabaro.com/shop-for-government/">Shop for Government</a>
-                       <a href="https://yabaro.com/oem/">OEM/ODM</a>
-                    </div>
-                </li>
-
-                <li class="">
-                    <div>Know About YABARO
-                        <i class="iconfont open-submenu" style="display: block;"></i>
-                        <i class="iconfont close-submenu" style="display: none;"></i>
-                    </div>
-                    <div class="sub-menu" style="display: none;">
-                       <a href="https://yabaro.com/about/">About Us</a>
-                       <a href="https://yabaro.com/job-opportunities/">Career</a>
-                       <a href="https://yabaro.com/contacting-yabaro">Contact Us</a>
-                       <a href="https://yabaro.com/recycle-program/">Recycle Program</a>
-                    </div>
+                <div>
+                 <a href="https://vaptiobeco.co.uk/contact">Contact Us</a>
+                   </div>
                 </li>
             </ul>
         </div>
@@ -103,7 +42,7 @@
 
         
         <div class="tips">
-         Copyright Notice © 2021 YABARO Holding Limited and/or its affiliates and licensors. All rights reserved.
+        Copyright Notice © 2021 BECO Holding Limited and/or its affiliates and licensors. All rights reserved.
         </div>
     </div>
 
