3331222ea5e6af1475b4fbea2c6587eef8b42733
kakajieshao <452652339@qq.com>
2021-10-01 09:16:06 +0800

    文件提交

---
+++ wp-content/themes/mixtapewp/media.php
@@ -0,0 +1,347 @@
+<?php /* Template Name: media */ ?>
+
+<?php get_header(); ?>
+
+
+<!--banner 开始-->
+<div class="return-refund_banner">
+    <div class="my-container" style="text-align:center;">
+        Hyppe Community
+    </div>
+</div>
+<!--banner 结束-->
+
+<!--中间内容 开始-->
+<div class="vapting-community-page">
+    <div class="my-container">
+        <!--quick start guide 开始-->
+        <div class="quick-guide">
+        <ul>
+                                <li class="video-pop" data-media="https://www.youtube.com/embed/87FCnforYe4" data-thum="http://yabaro.com/wp-content/uploads/2021/09/WechatIMG788.png" data-title=" Hyppe Bar Disposable Vape vs Puff Bar Review   ">
+                    <img class="img-shadow" src="http://yabaro.com/wp-content/uploads/2021/09/WechatIMG788.png" alt="https://www.youtube.com/embed/87FCnforYe4">
+                    <div class="video-con">
+                        <div class="icon-top">
+                            <i class="iconfont pause-btn iconsanjiao3"></i>
+                        </div>
+                        <p>play</p>
+                    </div>
+                    <div class="says-info">
+                        <p>
+                        Hyppe Bar Disposable Vape vs Puff Bar Review                      </p>
+                        <h6>
+                            <img src="http://yabaro.com/wp-content/uploads/2021/09/WechatIMG789.png" alt=""/>
+                            Fumb Duck                        </h6>
+                    </div>
+                </li>
+
+                <li class="video-pop" data-media="https://www.youtube.com/embed/GR_VnGo8XCE" data-thum="http://yabaro.com/wp-content/uploads/2021/09/WechatIMG798.png" data-title="IHYPPE MAX BANANA ICE DISPOSABLE VAPE REVIEW">
+                    <img class="img-shadow" src="http://yabaro.com/wp-content/uploads/2021/09/WechatIMG798.png" alt="https://www.youtube.com/embed/GR_VnGo8XCE">
+                    <div class="video-con">
+                        <div class="icon-top">
+                            <i class="iconfont pause-btn iconsanjiao3"></i>
+                        </div>
+                        <p>play</p>
+                    </div>
+                    <div class="says-info">
+                        <p>
+                        HYPPE MAX BANANA ICE DISPOSABLE VAPE REVIEW                      </p>
+                        <h6>
+                            <img src="http://yabaro.com/wp-content/uploads/2021/09/WechatIMG793.png" alt=""/>
+                            DisorderVapes                        </h6>
+                    </div>
+                </li>
+
+                <li class="video-pop" data-media="https://www.youtube.com/embed/0O4GClDMr2g" data-thum="http://yabaro.com/wp-content/uploads/2021/09/WechatIMG799.jpeg" data-title="Hyppe MAX FLOW | Gummy Teddy | Vape Review">
+                    <img class="img-shadow" src="http://yabaro.com/wp-content/uploads/2021/09/WechatIMG799.jpeg" alt="https://www.youtube.com/embed/0O4GClDMr2g">
+                    <div class="video-con">
+                        <div class="icon-top">
+                            <i class="iconfont pause-btn iconsanjiao3"></i>
+                        </div>
+                        <p>play</p>
+                    </div>
+                    <div class="says-info">
+                        <p>
+                        Hyppe MAX FLOW | Gummy Teddy | Vape Review       </p>
+                        <h6>
+                        <img src="http://yabaro.com/wp-content/uploads/2021/09/WechatIMG793.png" alt=""/>
+                            DisorderVapes                        </h6>
+                    </div>
+                </li>
+
+                <li class="video-pop" data-media="https://www.youtube.com/embed/YqKUXf2fJzA" data-thum="http://yabaro.com/wp-content/uploads/2021/09/1633038863294.jpg" data-title="Hyppe Max Flow Strawberry Freeze Disposable Vape Review!">
+                    <img class="img-shadow" src="http://yabaro.com/wp-content/uploads/2021/09/1633038863294.jpg" alt="https://www.youtube.com/embed/YqKUXf2fJzA">
+                    <div class="video-con">
+                        <div class="icon-top">
+                            <i class="iconfont pause-btn iconsanjiao3"></i>
+                        </div>
+                        <p>play</p>
+                    </div>
+                    <div class="says-info">
+                        <p>
+                        Hyppe Max Flow Strawberry Freeze Disposable Vape Review!</p>
+                        <h6>
+                        <img src="http://yabaro.com/wp-content/uploads/2021/09/WechatIMG800.jpeg" alt=""/>
+                        VaporWize Jackson
+                        </h6>
+                    </div>
+                </li>
+
+               
+                <li class="video-pop" data-media="https://www.youtube.com/embed/Pr14MIsRlqk" data-thum="http://yabaro.com/wp-content/uploads/2021/09/WechatIMG802.png" data-title="Hyppe Plus - Arome gustoase și perfect pentru urgențe">
+                    <img class="img-shadow" src="http://yabaro.com/wp-content/uploads/2021/09/WechatIMG802.png" alt="https://www.youtube.com/embed/Pr14MIsRlqk">
+                    <div class="video-con">
+                        <div class="icon-top">
+                            <i class="iconfont pause-btn iconsanjiao3"></i>
+                        </div>
+                        <p>play</p>
+                    </div>
+                    <div class="says-info">
+                        <p>
+                        Hyppe Plus - Arome gustoase și perfect pentru urgențe</p>
+                        <h6>
+                        <img src="http://yabaro.com/wp-content/uploads/2021/09/WechatIMG803.png" alt=""/>
+                        Vape Romania
+                        </h6>
+                    </div>
+                </li>
+
+                <li class="video-pop" data-media="https://www.youtube.com/embed/HeP3O6NPCKg" data-thum="http://yabaro.com/wp-content/uploads/2021/09/WechatIMG807.png" data-title="Hyppe Plus - Arome gustoase și perfect pentru urgențe">
+                    <img class="img-shadow" src="http://yabaro.com/wp-content/uploads/2021/09/WechatIMG807.png" alt="https://www.youtube.com/embed/HeP3O6NPCKg">
+                    <div class="video-con">
+                        <div class="icon-top">
+                            <i class="iconfont pause-btn iconsanjiao3"></i>
+                        </div>
+                        <p>play</p>
+                    </div>
+                    <div class="says-info">
+                        <p>
+                        HYPPE MAX FLOW 2000 PUFFS STRAWBERRY LEMONADE REVIEW!!!!</p>
+                        <h6>
+                        <img src="http://yabaro.com/wp-content/uploads/2021/09/WechatIMG808.png" alt=""/>
+                        Tanner P.
+                        </h6>
+                    </div>
+                </li>
+
+                <li class="video-pop" data-media="https://www.youtube.com/embed/mQ9ThSovPMA" data-thum="http://yabaro.com/wp-content/uploads/2021/09/WechatIMG812.png" data-title="HYPPE BAR LYCHEE SODA VAPE REVIEW">
+                    <img class="img-shadow" src="http://yabaro.com/wp-content/uploads/2021/09/WechatIMG812.png" alt="https://www.youtube.com/embed/mQ9ThSovPMA">
+                    <div class="video-con">
+                        <div class="icon-top">
+                            <i class="iconfont pause-btn iconsanjiao3"></i>
+                        </div>
+                        <p>play</p> 
+                    </div>
+                    <div class="says-info">
+                        <p>
+                        HYPPE BAR LYCHEE SODA VAPE REVIEW</p>
+                        <h6>
+                        <img src="http://yabaro.com/wp-content/uploads/2021/09/WechatIMG793.png" alt=""/>
+                        DisorderVapes </h6>
+                    </div>
+                </li>
+
+
+            
+
+                <li class="video-pop" data-media="https://www.youtube.com/embed/mQ9ThSovPMA" data-thum="http://yabaro.com/wp-content/uploads/2021/09/WechatIMG816.png" data-title="Hyppe MAX FLOW Disposable Vape Review">
+                    <img class="img-shadow" src="http://yabaro.com/wp-content/uploads/2021/09/WechatIMG816.png" alt="https://www.youtube.com/embed/mQ9ThSovPMA">
+                    <div class="video-con">
+                        <div class="icon-top">
+                            <i class="iconfont pause-btn iconsanjiao3"></i>
+                        </div>
+                        <p>play</p> 
+                    </div>
+                    <div class="says-info">
+                        <p>
+                        Hyppe MAX FLOW Disposable Vape Review</p>
+                        <h6>
+                        <img src="http://yabaro.com/wp-content/uploads/2021/09/WechatIMG819.png" alt=""/>
+                        onetakenate </h6>
+                    </div>
+                </li>
+
+
+                <li class="video-pop" data-media="https://www.youtube.com/embed/C8cezliJkIA" data-thum="http://yabaro.com/wp-content/uploads/2021/10/WechatIMG820.png" data-title="**NEW** Hype Bar MAX! (Grape Soda/ICE)">
+                    <img class="img-shadow" src="http://yabaro.com/wp-content/uploads/2021/10/WechatIMG820.png" alt="https://www.youtube.com/embed/C8cezliJkIA">
+                    <div class="video-con">
+                        <div class="icon-top">
+                            <i class="iconfont pause-btn iconsanjiao3"></i>
+                        </div>
+                        <p>play</p> 
+                    </div>
+                    <div class="says-info">
+                        <p>
+                        **NEW** Hype Bar MAX! (Grape Soda/ICE)</p>
+                        <h6>
+                        <img src="http://yabaro.com/wp-content/uploads/2021/10/WechatIMG821.png" alt=""/>
+                        Tallazfck </h6>
+                    </div>
+                </li>
+
+                <li class="video-pop" data-media="https://www.youtube.com/embed/qYRKxA7O3S8" data-thum="http://yabaro.com/wp-content/uploads/2021/10/WechatIMG824.png" data-title="HYPPE MAX FLOW - WATERMELON PEACH PEAR - VAPE REVIEW">
+                    <img class="img-shadow" src="http://yabaro.com/wp-content/uploads/2021/10/WechatIMG824.png" alt="https://www.youtube.com/embed/qYRKxA7O3S8">
+                    <div class="video-con">
+                        <div class="icon-top">
+                            <i class="iconfont pause-btn iconsanjiao3"></i>
+                        </div>
+                        <p>play</p> 
+                    </div>
+                    <div class="says-info">
+                        <p>
+                        HYPPE MAX FLOW - WATERMELON PEACH PEAR - VAPE REVIEW</p>
+                        <h6>
+                        <img src="http://yabaro.com/wp-content/uploads/2021/09/WechatIMG793.png" alt=""/>
+                        DisorderVapes </h6>
+                    </div>
+                </li>
+
+                <li class="video-pop" data-media="https://www.youtube.com/embed/OlbcfL3BaSc" data-thum="http://yabaro.com/wp-content/uploads/2021/10/WechatIMG833.png" data-title="HYPPE MAX 900mah 6ml 2000 затяжек и регулировка обдува">
+                    <img class="img-shadow" src="http://yabaro.com/wp-content/uploads/2021/10/WechatIMG833.png" alt="https://www.youtube.com/embed/OlbcfL3BaSc">
+                    <div class="video-con">
+                        <div class="icon-top">
+                            <i class="iconfont pause-btn iconsanjiao3"></i>
+                        </div>
+                        <p>play</p> 
+                    </div>
+                    <div class="says-info">
+                        <p>
+                        HYPPE MAX 900mah 6ml 2000 затяжек и регулировка обдува</p>
+                        <h6>
+                        <img src="http://yabaro.com/wp-content/uploads/2021/10/WechatIMG829.png" alt=""/>
+                        КОРОБКА </h6>
+                    </div>
+                </li>
+
+                <li class="video-pop" data-media="https://www.youtube.com/embed/GREB2vjRsL8" data-thum="http://yabaro.com/wp-content/uploads/2021/10/WechatIMG834.png" data-title="Hyppe Bar Max - Banana Ice">
+                    <img class="img-shadow" src="http://yabaro.com/wp-content/uploads/2021/10/WechatIMG834.png" alt="https://www.youtube.com/embed/GREB2vjRsL8">
+                    <div class="video-con">
+                        <div class="icon-top">
+                            <i class="iconfont pause-btn iconsanjiao3"></i>
+                        </div>
+                        <p>play</p> 
+                    </div>
+                    <div class="says-info">
+                        <p>
+                        Hyppe Bar Max - Banana Ice</p>
+                        <h6>
+                        <img src="http://yabaro.com/wp-content/uploads/2021/10/WechatIMG838.png" alt=""/>
+                        КОРОБКА </h6>
+                    </div>
+                </li>
+
+                <li class="video-pop" data-media="https://www.youtube.com/embed/bjtHc5cwZFw" data-thum="http://yabaro.com/wp-content/uploads/2021/10/WechatIMG839.png" data-title="BIGGEST DISPOSABLE VAPE EVER?!">
+                    <img class="img-shadow" src="http://yabaro.com/wp-content/uploads/2021/10/WechatIMG839.png" alt="https://www.youtube.com/embed/bjtHc5cwZFw">
+                    <div class="video-con">
+                        <div class="icon-top">
+                            <i class="iconfont pause-btn iconsanjiao3"></i>
+                        </div>
+                        <p>play</p> 
+                    </div>
+                    <div class="says-info">
+                        <p>
+                        BIGGEST DISPOSABLE VAPE EVER?!</p>
+                        <h6>
+                        <img src="http://yabaro.com/wp-content/uploads/2021/09/WechatIMG793.png" alt=""/>
+                        DisorderVapes </h6>
+                    </div>
+                </li>
+
+                <li class="video-pop" data-media="https://www.youtube.com/embed/6J9kArfU58Y" data-thum="http://yabaro.com/wp-content/uploads/2021/10/WechatIMG846.png" data-title="HYPPE ORANGE SODA VAPE REVIEW ">
+                    <img class="img-shadow" src="http://yabaro.com/wp-content/uploads/2021/10/WechatIMG846.png" alt="https://www.youtube.com/embed/6J9kArfU58Y">
+                    <div class="video-con">
+                        <div class="icon-top">
+                            <i class="iconfont pause-btn iconsanjiao3"></i>
+                        </div>
+                        <p>play</p> 
+                    </div>
+                    <div class="says-info">
+                        <p>
+                        HYPPE ORANGE SODA VAPE REVIEW </p>
+                        <h6>
+                        <img src="http://yabaro.com/wp-content/uploads/2021/10/WechatIMG845.png" alt=""/>
+                        David and Hunter Productions </h6>
+                    </div>
+                </li>
+
+                <li class="video-pop" data-media="https://www.youtube.com/embed/QUMvZGLuTJw" data-thum="http://yabaro.com/wp-content/uploads/2021/10/WechatIMG848.png" data-title="HYPPE MAX FLOW - GUAVA PINEAPPLE ORANGE - VAPE REVIEW">
+                    <img class="img-shadow" src="http://yabaro.com/wp-content/uploads/2021/10/WechatIMG848.png" alt="https://www.youtube.com/embed/QUMvZGLuTJw">
+                    <div class="video-con">
+                        <div class="icon-top">
+                            <i class="iconfont pause-btn iconsanjiao3"></i>
+                        </div>
+                        <p>play</p> 
+                    </div>
+                    <div class="says-info">
+                        <p>
+                        HYPPE MAX FLOW - GUAVA PINEAPPLE ORANGE - VAPE REVIEW</p>
+                        <h6>
+                        <img src="http://yabaro.com/wp-content/uploads/2021/09/WechatIMG793.png" alt=""/>
+                        DisorderVapes </h6>
+                    </div>
+                </li>
+
+
+
+                
+
+                         
+                            </ul>
+        </div>
+        <!--quick start guide 结束-->
+        <div class="m-pagination"></div>
+    </div>
+
+    <!--视频弹框 开始-->
+    <div class="guide-video">
+        <div class="shadow"></div>
+        <div class="dialog">
+            <h3></h3>
+        </div>
+    </div>
+    <!--视频弹框 结束-->
+</div>
+<!--中间结束-->
+
+
+<script>
+    
+    (function ($) {
+        var sourceDom = '';
+    $('.video-pop').on('click', function () {
+        var video_url = $(this).attr('data-media');
+        var img_url = $(this).attr('data-thum');
+        var title = $(this).attr('data-title');
+        $('.guide-video .dialog h3').html(title + '<i class="iconfont iconguanbi"></i>');
+        $('#video-box').attr('src', video_url);
+        sourceDom = '<iframe id="ytplayer" type="text/html" width="870" height="540"' +
+            'src="' + video_url + '"' +
+            'frameborder="0" allowfullscreen></iframe>';
+        $('.guide-video .dialog').append(sourceDom);
+        $('.guide-video .dialog').animate({
+            top: '50%'
+        }, 500);
+        $('#img-source').attr('src', img_url);
+        $('.guide-video').show();
+    });
+
+    $("body").on("click", ".iconguanbi", function() {
+        $('.guide-video .dialog iframe').remove();
+        $('.guide-video .dialog').animate({
+            top: '-35%'
+        }, 500).parents('.guide-video').hide({duration: 500})
+    });
+
+    function video_close() {
+        $('.guide-video .dialog iframe').remove();
+        $('.guide-video .dialog').animate({
+            top: '-35%'
+        }, 500).parents('.guide-video').hide({duration: 500})
+    }
+
+
+    })(jQuery);
+
+  
+</script>
+
+<?php get_footer(); ?>
