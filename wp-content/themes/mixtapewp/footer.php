d5e8dc5e1fc72a185b24624b01d13e38cef6fe60
kakajieshao <452652339@qq.com>
2021-10-08 17:56:51 +0800

    文件提交

--- wp-content/themes/mixtapewp/footer.php
+++ wp-content/themes/mixtapewp/footer.php
@@ -1,53 +1,64 @@
 <?php if(wp_is_mobile()): ?>
     <div class="site-footer">
         <div id="back-to-top">TOP <i class="iconfont"></i></div>
+         
         <div class="site-menu-m">
             <ul>
                 <li class="">
-                    <div>
-                        <a href="https://vaptiobeco.co.uk">Home</a>
+                    <div>Products
+                        <i class="iconfont open-submenu" style="display: block;"></i>
+                        <i class="iconfont close-submenu" style="display: none;"></i>
+                    </div>
+                    <div class="sub-menu" style="display: none;">
+                        <a href="http://hyppeuk.com/maxx">Maxx</a>
+                        <a href="http://hyppeuk.com/hyppe-q/">Q</a>
+                        <a href="http://hyppeuk.com/plus">Plus</a>
+                    </div>
+                </li>
+
+                <li>
+                    <div><b><a href="http://hyppeuk.com/about-us">Our Brand</a></b></div>
+                </li>
+
+                <li class="">
+                    <div>Community
+                        <i class="iconfont open-submenu" style="display: block;"></i>
+                        <i class="iconfont close-submenu" style="display: none;"></i>
+                    </div>
+                    <div class="sub-menu" style="display: none;">
+                        <a href="http://hyppeuk.com/media">Media</a>
+                        <a href="http://hyppeuk.com/news">News</a>
                     </div>
                 </li>
 
                 <li class="">
-                <div>
-                    <a href="https://vaptiobeco.co.uk/about-us">About Us</a>
+                    <div>Support
+                        <i class="iconfont open-submenu" style="display: block;"></i>
+                        <i class="iconfont close-submenu" style="display: none;"></i>
                     </div>
-                </li>
-                <li class="">
-                <div>
-                     <a href="https://vaptiobeco.co.uk/partner">Partners</a>
-                     </div>
-                </li>
-                <li class="">
-                <div>
-                    <a href="https://vaptiobeco.co.uk/wheretobuy">Where to Buy </a>
+                    <div class="sub-menu" style="display: none;">
+                        <a href="http://hyppeuk.com/media">Download</a>
+                        <a href="http://hyppeuk.com/news">Faq</a>
                     </div>
                 </li>
 
-                <li class="">
-                <div>
-                   <a href="https://vaptiobeco.co.uk/faq">FAQ</a>
-                   </div>
-                </li>
-
-                <li class="">
-                <div>
-                 <a href="https://vaptiobeco.co.uk/contact">Contact Us</a>
-                   </div>
+                <li>
+                    <div><b><<a href="http://hyppeuk.com/contact"><b>Contact us</b></a></b></div>
                 </li>
             </ul>
         </div>
-        <form class="tnp-form newsletter" action="https://maxcorevape.com/?na=s" method="post" _lpchecked="1"><input type="hidden" name="nr" value="widget-minimal"><input class="tnp-email" type="email" required="" name="ne" value="" placeholder="Email"><input style="display:none;" class="tnp-submit" type="submit" value="Subscribe"><button id="subscribe" data-can="1">Submit</button></form>
+
+
+
+        <form class="tnp-form newsletter" action="http://hyppeuk.com/?na=s" method="post" _lpchecked="1"><input type="hidden" name="nr" value="widget-minimal"><input class="tnp-email" type="email" required="" name="ne" value="" placeholder="Email"><input style="display:none;" class="tnp-submit" type="submit" value="Subscribe"><button id="subscribe" data-can="1">Submit</button></form>
 
         
         <div class="tips">
-        Copyright Notice © 2021 BECO Holding Limited and/or its affiliates and licensors. All rights reserved.
+        Copyright Notice © 2021 HYPPE Holding Limited and/or its affiliates and licensors. All rights reserved
         </div>
     </div>
 
     <script src="<?php echo get_template_directory_uri();?>/assets/js/header-footer-m.js" type="text/javascript" charset="utf-8"></script>
-
 
 
 <?php else: ?>
