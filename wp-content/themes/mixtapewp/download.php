b9f22d1d2d0cca52b55fc0d0a06177fd65d96655
kakajieshao <452652339@qq.com>
2021-10-11 17:13:33 +0800

    文件提交

---
+++ wp-content/themes/mixtapewp/download.php
@@ -0,0 +1,15 @@
+<?php /* Template Name: faq */ ?>
+
+<?php get_header(); ?>
+
+<!--banner 开始-->
+<div class="return-refund_banner">
+    <div class="my-container" style="text-align:center;">
+    DOWNLOAD
+    </div>
+</div>
+<!--banner 结束-->
+
+
+
+<?php get_footer(); ?>
